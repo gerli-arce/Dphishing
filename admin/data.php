866a101ab57de91986972f0271c94122e5806789
yopirata <yoninfantearce@gmail.com>
2021-09-10 12:29:45 -0500

    gaa

---
+++ admin/data.php
@@ -0,0 +1,57 @@
+<?php 
+$data = file_get_contents('../usernames.json');
+
+$array = json_decode($data, true);
+
+echo $data;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    xd
+</body>
+</html>
